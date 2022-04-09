eb193fdb5561c6d521046a235e1ab96e2d90a9dd
Miles Grist <39013281+Smiler-Miles@users.noreply.github.com>
2022-04-09 13:32:16 +0100

    initial setup of starter code

---
+++ portfolio-site/pages/projects/first-project.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function FirstProject() {
+    return (
+    <>
+    <h1>First Project</h1>
+    <h2>
+        <Link href="/">
+            <a>Home</a>
+        </Link>
+    </h2>
+    </>
+    )
+}
